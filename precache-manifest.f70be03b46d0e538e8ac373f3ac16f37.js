3c55e61d9c5579ee81faba0de1b2627f1d8067a4
Guersom92 <guersom80@gmail.com>
2020-08-04 09:28:15 -0500

    Updates

---
+++ precache-manifest.f70be03b46d0e538e8ac373f3ac16f37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32516c3385735441f3059d9f9fb55341",
+    "url": "/CalculadoraDeTiempo/index.html"
+  },
+  {
+    "revision": "2c454ed2eba456df5bee",
+    "url": "/CalculadoraDeTiempo/static/css/main.88b4da1a.chunk.css"
+  },
+  {
+    "revision": "a83434c9b2c891bcec24",
+    "url": "/CalculadoraDeTiempo/static/js/2.d21c4ab1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CalculadoraDeTiempo/static/js/2.d21c4ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c454ed2eba456df5bee",
+    "url": "/CalculadoraDeTiempo/static/js/main.9e0b1ec4.chunk.js"
+  },
+  {
+    "revision": "3b7b7aa390bd1b4d7efd",
+    "url": "/CalculadoraDeTiempo/static/js/runtime-main.c373e3c6.js"
+  }
+]);
