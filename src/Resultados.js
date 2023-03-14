77f2a8c3c5dd5abf4e6a41c492a013fda6ef1a14
Guersom92 <guersom80@gmail.com>
2023-03-13 23:30:21 -0500

    Haciendo que los puntos sean continuos

--- src/Resultados.js
+++ src/Resultados.js
@@ -1,25 +1,33 @@
 import React from "react";
 
 function calcularSemanas(cantidad) {
-  return (Math.floor((cantidad * 365) / 7));
+  return Math.floor((cantidad * 365) / 7);
 }
 
 function Resultados({ añosRestantes, añosVividos }) {
   const semanasRestantes = [];
   const semanasVividas = [];
   for (let index = 0; index < (añosVividos * 365) / 7; index++) {
-    semanasVividas.push(<span class="dot"></span>);
+    semanasVividas.push(<span className="diasVividos" key={index}></span>);
   }
   for (let index = 0; index < (añosRestantes * 365) / 7; index++) {
-    semanasRestantes.push(<span class="dot"></span>);
+    semanasRestantes.push(<span className="diasRestantes" key={index}></span>);
   }
 
   if (añosRestantes)
     return (
       <div className="resultados">
         <p>Te quedan {calcularSemanas(añosRestantes)} semanas</p>
-        <div className="diasVividos"> <div className="nubeVividos">{calcularSemanas(añosVividos)} Semanas vividas</div> {semanasVividas}</div>
-        <div className="diasRestantes"> <div className="nubeRestantes">{calcularSemanas(añosRestantes)} Semanas Restantes</div> {semanasRestantes}</div>
+        <div className="contenedorDias">
+          {semanasVividas}
+          <div className="nubeVividos">
+            {calcularSemanas(añosVividos)} Semanas vividas
+          </div>
+          {semanasRestantes}
+          <div className="nubeRestantes">
+            {calcularSemanas(añosRestantes)} Semanas Restantes
+          </div>
+        </div>
       </div>
     );
   else return null;
